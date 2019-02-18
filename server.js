96f35b310fbfc55652e0138cfabf64ad805deb93
s150915 <s150915@berufskolleg-borken.de>
2019-02-18 14:29:42 +0100

    ...

--- server.js
+++ server.js
@@ -15,21 +15,38 @@ const express = require('express')
 const app = express()
 
 // Mit der ejs-View-Engine werden Werte von der server.js zur index-Datei gegeben.
+// Muss wieder im Terminal installiert werden (npm install ejs --save)
 
 app.set('view engine', 'ejs')
 
-// 
+/* Gibt an, wo meine statischen Inhalte sind. 
+Sucht diese im Ordner Public. */
 
 app.use(express.static('public'))
 
-// ...
+/* Der Wert darf sich während der Programmlaufzeit nicht ändern.
+Bereitet die Daten zur Übergabe an die server.js vor.
+Dann npm installieren (npm install --save)*/
 
 const bodyParser = require('body-parser')
 
-// ...
+// Bindet den BodyParser ein. Der Wert soll wahr sein.
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-// ...
+// Startet den Server. 
+// Der Server lauscht auf Befehle vom Client (deswegen listen)
+// Port kann vom Dienst selbst ausgesucht werden. 
+/* Unser Rechner hat Port 3000 vorgegeben. Im Browser muss die 3000 angegeben 
+werden. */
 
-const server = app.listen(process.env.PORT || 3000, () => {        console.log('Server lauscht auf Port %s', server.address().port)    })
+const server = app.listen(process.env.PORT || 3000, () => {        
+    console.log('Der Server ist erfolgreich gestartet auf Port %s', server.address().port)   
+})
+
+// Server kann durch GET-Request erreicht werden.
+
+app.get('/',(req, res, next) => {
+    res.render('index.ejs', {
+    })
+}) 
