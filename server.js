3c60a58ebb17412dd59897cd7dd755076be23c64
s150915 <s150915@berufskolleg-borken.de>
2019-09-13 09:10:21 +0200

    ...

--- server.js
+++ server.js
@@ -40,6 +40,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const app = express()
+const mysql = require('mysql') 
+const dbVerbindung = mysql.createConnection({
+    host: '10.40.38.110',
+    user: 'placematman',
+    password: 'BKB123456!',
+    db: 'dbn27',
+    port: '3306'
+})
+
+dbVerbindung.connect();
+
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
@@ -159,6 +171,13 @@ app.post('/kontoAnlegen',(req, res, next) => {
         const laenderkennung = "DE"
         konto.Iban = iban.fromBBAN(laenderkennung,bankleitzahl + " " + konto.Kontonummer)
         
+        //Füge das Konto in die Mysql- Datenbank ein.
+
+        dbVerbindung.query('INSERT INTO konto(iban,timestamp,anfangssaldo,kontoart) VALUES("'+ konto.Iban +'",now(),100,"'+ konto.Kontoart +'")', function (error, results, fields) {
+            if (error) throw error;
+            console.log('Das Konto wurde erfolgreich angelegt '); 
+          });
+           
 
         // ... wird die kontoAnlegen.ejs gerendert.
 
